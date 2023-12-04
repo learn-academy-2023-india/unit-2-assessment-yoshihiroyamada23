3afb747a141c8d04036c2834eedfeaa36c803274
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2023-12-04 10:04:43 -0800

    finished unit 2 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,40 +13,94 @@
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
 // a) Create a test with expect statements for each of the variables provided.
+// describe("multiplyByTHree", () =>{
+//     it("is a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3", () =>{
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
+// // Expected output: [72, 81, 90, 99, 108]
+// expect(multiplyByThree(numbersArray1)).toEqual([18, 21, 24, 27, 30])
+// expect(multiplyByThree(numbersArray2)).toEqual([72, 81, 90, 99, 108])
+// })
+// })
+// // b) Create the function that makes the test pass.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// // Pseudo code:
+// // input:single array of numbers
+// // output: array with numbers mult by 3
+// //do something to every item in the array .map
+// //value * 3
+// const multiplyByThree = (array) => {
+//     return array.map((value) => {
+//       return value * 3
+//     })
+//   }
+// // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
-// b) Create the function that makes the test pass.
+// // a) Create a test with expect statements for each of the variables provided.
+// //test
+// describe("divisibleByThree", () =>{
+//     it("Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.", () =>{
+//         const object1 = { number: 15 }
+//         // Expected output: "15 is divisible by three"
+//         const object2 = { number: 0 }
+//         // Expected output: "0 is divisible by three"
+//         const object3 = { number: -7 }
+//         // Expected output: "-7 is not divisible by three"
+// expect(divisibleByThree(object1)).toEqual("15 is divisible by three")
+// expect(divisibleByThree(object2)).toEqual("0 is divisible by three")
+// expect(divisibleByThree(object3)).toEqual("-7 is not divisible by three")
+// })
+// })
 
-// Pseudo code:
+// // b) Create the function that makes the test pass.
 
-// --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+// // Pseudo code:
+// //input:create a function to see if the number inside the argument is divisible by three or not
+// //output:arguments divide by three
+// //use if, else if, else statement to decide if object 1-3 are divisible by three or not
 
-// a) Create a test with expect statements for each of the variables provided.
-
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
-
-// b) Create the function that makes the test pass.
-
-// Pseudo code:
+// const divisibleByThree = (obj) =>{
+//     //to get the number of the object have to use .number
+//     if(obj.number % 3 === 0){
+//         return `${obj.number} is divisible by three`
+//     }else if (obj.number % 3 !== 0)
+//         return  `${obj.number} is not divisible by three`
+// }
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+describe("arrayOfWords", () =>{
+    it("Create a function that takes in an array of words and returns an array with all the words capitalized.", () =>{
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+        // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+expect(arrayOfWords(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+expect(arrayOfWords(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+})
+})
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+//input:takes in an array of words
+//output: return all of the words capitalized
+//.toUpperCase only the 1st index
+//.map to map arrays
+//.split to split the wordings
+//.join to join the splitted words for the capitalization
+
+const arrayOfWords =(array) =>{
+    return array.map(value =>{
+        return value.split ("").map((value,index) =>{
+            if (index === 0){
+                return value.toUpperCase()
+            }else {
+                return value
+            }
+            
+        }).join("")
+    })
+}
--- interview-questions.md
+++ interview-questions.md
@@ -11,31 +11,31 @@ Type your initials here to confirm you read the instructions:
 1. What is the difference between a parameter and an argument?
 
 Your answer:
-
+a parameter is one where you code for example const insideTheParenthesisIs(parameter) and an argument would be where you define what parameter would be. Example. insideTheParanthesisIs(param1, param2)
 Researched answer:
-
+A parameter is one of the variables in a function. When a method is called, the arguments are the data you passed into the method's parameters.
 2. What is the difference between an object and a class?
 
-Your answer:
+Your answer:object have a symbol and a unique identifier. {key:"value}. where a class makes object reusable and has a template or a blueprint or recipe for others.
 
-Researched answer:
+Researched answer: a class is a template for objects. They define object properties that include valid range of values and a default value. Object is a member of a class.
 
 3. What is iteration?
 
-Your answer:
+Your answer: something that is repetetive
 
-Researched answer:
+Researched answer: Occurs when we want to execute code once for each item in a collection, usually elements in an array or properties of an object.
 
 4. STRETCH: What is hoisting in JavaScript?
 
-Researched answer:
+Researched answer: The process whereby the interpreter appeares to move the declaration of functions, variables, classes, or imports to the top of their scope, prior execution of the code.
 
 ## Looking Ahead: Terms for Next Unit
 
 Research and define the following terms. Hint: use the syllabus!
 
-1. React State:
+1. React State: React state is used to store data within a react component. Data stored in state is available to the entire react component. useState() is method specific to react that defines a variable and its initial vallue as well as creating a method that can be called to update that value as needed.
 
-2. React Props:
+2. React Props:Props (properties) are specialized React objects used to pass information from one component to another. Props create a unidirectional flow of data and behavior from one component down to a nested component via the component invocation. While state values are constantly updated through user interactions, props cannot be changed. They are a "read only" value.
 
-3. JSX:
+3. JSX: Javascript XML- marriage of JavaScript and HTML, JSX is what is returned from what is returned from every component, allows us to insert JavaScript logic to the markup.
